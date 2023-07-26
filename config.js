6c62d80dcd4cb8eda72e18ae2d6e0f9c584c5d3b
tkhe19 <tahsin.kheya@brilliant.com.bd>
2023-07-26 10:45:20 +0600

    added docker stuff

--- config.js
+++ config.js
@@ -1,1 +1,4 @@
-export const API_BASE_URL = "http://127.0.0.1:5000/api/v1";
+export const API_BASE_URL =
+  window.location.hostname === "36.255.70.170"
+    ? "http://36.255.70.170:8000/api/v1"
+    : "http://127.0.0.1:5000/api/v1";
